a855b68aac5d8116214154d07a0cc40c590a7ffd
Jens Andreasson <jens.andreasson@ntig.se>
2025-01-14 11:14:58 +0100

    feat: enhance tweet display with relative time formatting and update page title

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,11 @@ router.get("/", async (req, res) => {
   const [tweets] = await pool.promise().query(`
     SELECT tweet.*, user.name, DATE_FORMAT(tweet.updated_at, "%Y-%m-%d %H:%i") AS date
     FROM tweet
-    JOIN user ON tweet.author_id = user.id;`)
+    JOIN user ON tweet.author_id = user.id
+    ORDER BY updated_at DESC;`)
 
   res.render("index.njk", {
-    title: "Qvixter",
+    title: "Qvixter - All posts",
     message: "Message from routes/index.js",
     tweets: tweets,
   })
